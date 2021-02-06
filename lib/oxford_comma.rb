511bb1f1dbc878377d83cf5de98fd4c78eeed7e3
Catherine Loesch <catherine.loesch@protonmail.com>
2021-02-06 11:33:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        string = array.join
+        string
+    elsif array.size == 2
+        string = array.join(" and ")
+        string
+    else
+        last_item = array[-1]
+        array.pop
+        array << "and #{last_item}"
+        string = array.join(", ")
+        string
+    end
 end
